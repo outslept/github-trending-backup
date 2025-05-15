e1ebe655ab3c8d78f315e654c2df97bec4adb210
outslept <webeugene@proton.me>
2025-05-15 22:12:58 +0300

    chore: fix

--- packages/github-trending-tools/src/utils/git.ts
+++ packages/github-trending-tools/src/utils/git.ts
@@ -1,17 +1,19 @@
 import type { SimpleGit } from 'simple-git'
 import process from 'node:process'
+import { join } from 'pathe'
 import simpleGit from 'simple-git'
 import { logger } from './logger'
 
+const rootDir = join(process.cwd(), '..', '..')
 const git: SimpleGit = simpleGit({
-  baseDir: process.cwd(),
+  baseDir: rootDir,
   binary: 'git',
   maxConcurrentProcesses: 6,
 })
 
 export async function commit(): Promise<void> {
   try {
-    await git.add('.')
+    await git.add('data/*')
     await git.commit('Update trending repositories [skip ci]')
     logger.success('Changes committed')
   }
@@ -44,7 +46,8 @@ export async function push(remote = 'origin', branch = 'master'): Promise<void> 
 
 export async function hasChanges(): Promise<boolean> {
   try {
-    return !(await git.status()).isClean()
+    const status = await git.status()
+    return !status.isClean()
   }
   catch (err) {
     logger.error('Status check failed:', err)
