1d55811b7fa5a01849b275b07dd06f8de13095c1
outslept <webeugene@proton.me>
2025-05-20 21:56:04 +0300

    feat: Removing the stuff I don't really want anymore

--- packages/github-trending-inspector/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '$/keys'
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
-import * as schema from './schema'
-
-const sql = neon(env.DATABASE_URL)
-export const db = drizzle(sql, { schema })
