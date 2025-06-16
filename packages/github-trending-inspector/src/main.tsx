79c7121a269e98f9e5be6f2e32375a9af5da53ed
outslept <webeugene@proton.me>
2025-06-16 17:01:44 +0300

    feat: setting up the rest

---
+++ packages/github-trending-inspector/src/main.tsx
@@ -0,0 +1,23 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { DesignSystemProvider } from "./providers";
+import { routeTree } from "./routeTree.gen";
+import "./styles.css";
+import "./fonts.css";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.querySelector("#root")!).render(
+  <StrictMode>
+    <DesignSystemProvider>
+      <RouterProvider router={router} />
+    </DesignSystemProvider>
+  </StrictMode>,
+);
