ab6ac562291338120eb7a152b826c8b8a4812c98
outslept <webeugene@proton.me>
2025-06-22 05:58:12 +0300

    chore: autogen routes

--- packages/github-trending-inspector/src/routeTree.gen.ts
+++ packages/github-trending-inspector/src/routeTree.gen.ts
@@ -1,4 +1,5 @@
-/* eslint-disable unicorn/filename-case */
+/* eslint-disable */
+
 // @ts-nocheck
 
 // noinspection JSUnusedGlobalSymbols
@@ -7,70 +8,70 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from "./routes/__root";
-import { Route as DateRouteImport } from "./routes/$date";
-import { Route as IndexRouteImport } from "./routes/index";
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as DateRouteImport } from './routes/$date'
+import { Route as IndexRouteImport } from './routes/index'
 
 const DateRoute = DateRouteImport.update({
-  id: "/$date",
-  path: "/$date",
+  id: '/$date',
+  path: '/$date',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const IndexRoute = IndexRouteImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute;
-  "/$date": typeof DateRoute;
+  '/': typeof IndexRoute
+  '/$date': typeof DateRoute
 }
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/$date": typeof DateRoute;
+  '/': typeof IndexRoute
+  '/$date': typeof DateRoute
 }
 export interface FileRoutesById {
-  __root__: typeof rootRouteImport;
-  "/": typeof IndexRoute;
-  "/$date": typeof DateRoute;
+  __root__: typeof rootRouteImport
+  '/': typeof IndexRoute
+  '/$date': typeof DateRoute
 }
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
-  fullPaths: "/" | "/$date";
-  fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "/$date";
-  id: "__root__" | "/" | "/$date";
-  fileRoutesById: FileRoutesById;
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/$date'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/$date'
+  id: '__root__' | '/' | '/$date'
+  fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  DateRoute: typeof DateRoute;
+  IndexRoute: typeof IndexRoute
+  DateRoute: typeof DateRoute
 }
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/$date": {
-      id: "/$date";
-      path: "/$date";
-      fullPath: "/$date";
-      preLoaderRoute: typeof DateRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
+    '/$date': {
+      id: '/$date'
+      path: '/$date'
+      fullPath: '/$date'
+      preLoaderRoute: typeof DateRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
   }
 }
 
 const rootRouteChildren: RootRouteChildren = {
-  IndexRoute,
-  DateRoute,
-};
+  IndexRoute: IndexRoute,
+  DateRoute: DateRoute,
+}
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
+  ._addFileTypes<FileRouteTypes>()
