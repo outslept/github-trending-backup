f4a228cde28513c1736b19bb45c28f239788d07d
outslept <webeugene@proton.me>
2025-06-24 05:56:17 +0300

    chore: vite config

--- packages/github-trending-inspector/vite.config.ts
+++ packages/github-trending-inspector/vite.config.ts
@@ -1,15 +1,10 @@
-import path from "node:path";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [tanstackRouter(), react(), tailwindcss()],
   server: {
     port: 3000,
   },
