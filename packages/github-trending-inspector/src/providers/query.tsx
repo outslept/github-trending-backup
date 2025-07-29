3ae6805685b1a9d00ff90eaf65b7d5a2591cfb1e
outslept <135520429+outslept@users.noreply.github.com>
2025-07-29 04:41:47 +0300

    feat: add react query devtools

--- packages/github-trending-inspector/src/providers/query.tsx
+++ packages/github-trending-inspector/src/providers/query.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { ReactNode } from 'react';
 
 const queryClient = new QueryClient({
@@ -12,6 +13,9 @@ const queryClient = new QueryClient({
 
 export function QueryProvider({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+      </QueryClientProvider>
   );
 }
